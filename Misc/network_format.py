340f73923263289aae70486c6fa4d59aef050031
Alfred Reich <al.reich@gmail.com>
2020-09-10 22:19:14 -0500

    Setup some example formats for RCC8

---
+++ Misc/network_format.py
@@ -0,0 +1,32 @@
+# Test format for Temporal Networks
+
+# 
+# 
+# https://en.wikipedia.org/wiki/Region_connection_calculus#Examples,
+
+net = {
+    "name": "RCC8 Example",
+    "algebra": "RCC8",
+    "nodes": {
+        "h1": {
+            "name": "House1",
+            "class": "Region"
+            },
+        "h2": {
+            "name": "House2",
+            "class": "Region"
+            },
+        "p1": {
+            "name": "Property1",
+            "class": "Region"
+            }
+        "p2": {
+            "name": "Property2",
+            "class": "Region"
+            }
+        "r": {
+            "name": "Road",
+            "class": "Region"
+            },
+        }
+    }
